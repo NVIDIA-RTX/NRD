16a64cd4daebfb153590623f0348b36007284ba3
dzhdan <dzhdan@nvidia.com>
2025-03-20 10:18:08 +0800

    v4.14.3: - CMake: tagged specific MathLib release

--- Include/NRD.h
+++ Include/NRD.h
@@ -29,8 +29,8 @@ CREDITS:
 
 #define NRD_VERSION_MAJOR 4
 #define NRD_VERSION_MINOR 14
-#define NRD_VERSION_BUILD 2
-#define NRD_VERSION_DATE "19 March 2025"
+#define NRD_VERSION_BUILD 3
+#define NRD_VERSION_DATE "20 March 2025"
 
 #if defined(_WIN32)
     #define NRD_CALL __stdcall
