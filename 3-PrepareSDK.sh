a1c1e0e84b49058006449499618e9e3fe75df34b
dzhdan <dzhdan@nvidia.com>
2025-12-22 11:34:36 +0800

    Adjusted script

--- 3-PrepareSDK.sh
+++ 3-PrepareSDK.sh
@@ -1,31 +1,27 @@
 #!/bin/bash
 
 ROOT=$(pwd)
-SELF=$(dirname "$0")
+SELF=$(cd "$(dirname "$0")" && pwd)
 
 rm -rf "_NRD_SDK"
-mkdir -p "_NRD_SDK"
-cd "_NRD_SDK"
 
-mkdir -p "Include"
-mkdir -p "Integration"
-mkdir -p "Lib/Debug"
-mkdir -p "Lib/Release"
-mkdir -p "Shaders"
+mkdir -p "_NRD_SDK/Include"
+mkdir -p "_NRD_SDK/Integration"
+mkdir -p "_NRD_SDK/Lib/Debug"
+mkdir -p "_NRD_SDK/Lib/Release"
+mkdir -p "_NRD_SDK/Shaders"
 
-cp -r "${SELF}/Include/." "Include"
-cp -r "${SELF}/Integration/." "Integration"
-cp -r "${SELF}/Shaders/Include/NRD.hlsli" "Shaders"
-cp -r "${SELF}/Shaders/Include/NRDConfig.hlsli" "Shaders"
-cp "${SELF}/LICENSE.txt" "."
-cp "${SELF}/README.md" "."
-cp "${SELF}/UPDATE.md" "."
+cp -r "${SELF}/Include/." "_NRD_SDK/Include"
+cp -r "${SELF}/Integration/." "_NRD_SDK/Integration"
+cp "${SELF}/Shaders/Include/NRD.hlsli" "_NRD_SDK/Shaders"
+cp "${SELF}/Shaders/Include/NRDConfig.hlsli" "_NRD_SDK/Shaders"
+cp "${SELF}/LICENSE.txt" "_NRD_SDK/"
+cp "${SELF}/README.md" "_NRD_SDK/"
+cp "${SELF}/UPDATE.md" "_NRD_SDK/"
 
-cp -H "${ROOT}/_Bin/Debug/libNRD.so" "Lib/Debug"
-cp -H "${ROOT}/_Bin/Release/libNRD.so" "Lib/Release"
-
-cd ..
+cp -H "${ROOT}/_Bin/Debug/libNRD.so" "_NRD_SDK/Lib/Debug"
+cp -H "${ROOT}/_Bin/Release/libNRD.so" "_NRD_SDK/Lib/Release"
 
 if [ -f "_Build/_deps/nri-src/3-PrepareSDK.sh" ]; then
-    source "_Build/_deps/nri-src/3-PrepareSDK.sh"
+    bash "_Build/_deps/nri-src/3-PrepareSDK.sh"
 fi
